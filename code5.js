82305331625fdca1f652ad8c1d6e8adb0e5bb3fc
Andressa Abrantes <andressasabrantes@gmail.com>
2023-03-16 21:37:08 -0300

    DevAprender: FizzBuzz

--- code5.js
+++ code5.js
@@ -8,17 +8,13 @@ function fizzBuzz(entrada) {
     
     if (typeof entrada !== "number") {
         return "Não é um número!"
-    }
-        else if ((entrada % 3 === 0) && (entrada % 5 === 0)) {
-            return "FizzBuzz"
+    } else if ((entrada % 3 === 0) && (entrada % 5 === 0)) {
+        return "FizzBuzz"
+    } else if (entrada % 3 === 0) {
+        return "Fizz"
+    } else if (entrada % 5 === 0) {
+        return "Buzz"
     } 
-        else if (entrada % 3 === 0) {
-            return "Fizz"
-    } 
-        else if (entrada % 5 === 0) {
-            return "Buzz"
-    } 
-
     return entrada
 }
 
