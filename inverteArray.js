6a9a0dfc6f7e056975d74fe1b12e9675f4c7febe
Andressa Abrantes <andressasabrantes@gmail.com>
2023-03-18 18:49:55 -0300

    Lab: Adicionando novos testes

--- inverteArray.js
+++ inverteArray.js
@@ -11,4 +11,7 @@ function inverteArray(entrada) {
     return arrayInvertido;
 }
 console.log(inverteArray([1, 2, 3, 4]))//[4,3,2,1]
+console.log(inverteArray([1, 2, 3, 4, 5, 6]))//[6,5,4,3,2,1]
+console.log(inverteArray(["S", "J", 3, "k", 5]))//[5,'k',3,'J','S']
+console.log(inverteArray(["Car", 78, "Fridge", 50, "456", 82]))//[82,'456',50,'Fridge',78,'Car']
 
