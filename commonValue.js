bae4c36a84cdcab771618c61c0a119aaf85608e4
Andressa Abrantes <andressasabrantes@gmail.com>
2023-03-27 19:50:53 -0300

    Lab: Encontrar valores em comum entre dois arrays

---
+++ commonValue.js
@@ -0,0 +1,21 @@
+/* 10) Implemente um método que encontre os valores comuns entre dois arrays.
+Entrada do método ([6, 8], [8, 9]), Resultado do método: [8] */
+
+function isCommon(array1, array2) {
+
+    let commonValues = []
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) {
+                commonValues.push(array1[i])
+            }
+
+        }
+    }
+    
+    return commonValues
+}
+console.log(isCommon([6, 8], [8, 9]))//[8]
+console.log(isCommon(['6', 8, 9], [6, 9]))//[9]
+console.log(isCommon(['6', 8, 48, 'Hey!', 75, 10, 11, '25'], [25, '6', 6, 48, 75, 'Hello', 'Olá', 'Hey!']))//['6', 48, 'Hey!', 75]
+console.log(isCommon([0, 100, 96, true, 'verdadeiro', false, '90'], [0, '0', '100', true, 79, 'verdadeiro', 'false', 90, 100]))//[0, 100, true, verdadeiro]
