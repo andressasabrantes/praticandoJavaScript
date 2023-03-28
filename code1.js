50fffe89ff266ba3897779bde2a37e556d9b2511
Andressa Abrantes <andressasabrantes@gmail.com>
2023-03-27 21:47:06 -0300

    leetCode: Somar elementos do array e comparar com o segundo parâmetro

--- code1.js
+++ code1.js
@@ -11,14 +11,18 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 
 
+function twoSums(array, target) {
 
-function twoSum(nums, target) {
-    let result = 0
-    for (let i = 0; i < nums.lenght; i++) {
-        result += nums[i]
-            if (result === target) {
-                console.log(result)
-            }
+   for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] == target) {
+            return [i, j]
+        }
     }
+   }
 }
-console.log(twoSum([2,7,11,15], 9))
+
+console.log(twoSums([2,7,11,15], 9))//[0,1]
+console.log(twoSums([2,7,11,15], 18))//[1,2]
+console.log(twoSums([15, 25, 8, 90], 33))//[1,2]
+console.log(twoSums([15, 25, 8, 90], 98))//[2,3]
